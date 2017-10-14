0397aa07d18b4c2b91f6f5c02fd4dfd1821bc0a1
Adrian Morente <adrian95morente@gmail.com>
2017-10-15 01:18:12 +0200

    añadiendo modelo de usuario

---
+++ models/user.js
@@ -0,0 +1,16 @@
+var User = {
+  firstname: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  gh-username: {
+    type: String,
+    required: true
+  }
+};
+
+module.exports = User;
