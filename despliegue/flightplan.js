7f6054c413a4ae538552ba071386aeb90da7bee7
Adrian Morente <adrian95morente@gmail.com>
2018-01-05 21:07:18 +0100

    fixing

--- despliegue/flightplan.js
+++ despliegue/flightplan.js
@@ -4,7 +4,7 @@ var username = 'vagrant';
 
 // configuration
 plan.target('staging', [{
-  host: 'billowing-brook-76.westus.cloudapp.azure.com',
+  host: 'bold-grass-66.westus.cloudapp.azure.com',
   username: username,
   agent: process.env.SSH_AUTH_SOCK
 }]);
@@ -14,6 +14,6 @@ plan.remote(function(remote) {
   remote.log('Ejecutando microservicio...');
   remote.with('cd ~/PracticasIV', function(){
     remote.exec('npm install');
-    remote.sudo('pm2 start app.js')
+    remote.exec('sudo pm2 start app.js')
   });
 });
