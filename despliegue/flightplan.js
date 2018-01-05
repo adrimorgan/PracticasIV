e4544b12ea506dd1be58c84c49c16fbaa387acd7
Adrian Morente <adrian95morente@gmail.com>
2018-01-05 20:58:05 +0100

    cambiando método de despliegue (docker no funciona)

--- despliegue/flightplan.js
+++ despliegue/flightplan.js
@@ -3,15 +3,17 @@ var plan = require('flightplan');
 var username = 'vagrant';
 
 // configuration
-plan.target('production', [
-  {
-    host: 'billowing-brook-76.westus.cloudapp.azure.com',
-    username: username,
-    agent: process.env.SSH_AUTH_SOCK
-  }
-]);
+plan.target('staging', [{
+  host: 'billowing-brook-76.westus.cloudapp.azure.com',
+  username: username,
+  agent: process.env.SSH_AUTH_SOCK
+}]);
 
 // run commands on remote hosts (destinations)
 plan.remote(function(remote) {
-  remote.sudo('docker run adrianmorente/practicasiv');
+  remote.log('Ejecutando microservicio...');
+  remote.with('cd ~/PracticasIV', function(){
+    remote.exec('npm install');
+    remote.sudo('pm2 start app.js')
+  });
 });
