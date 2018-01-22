14f018e0220ee42951e4e381be31350da08facaf
Adrian Morente <adrian95morente@gmail.com>
2018-01-22 21:05:34 +0100

    probando JWTs

--- app.js
+++ app.js
@@ -1,55 +1,63 @@
 #!/usr/bin/node
 var express = require('express');
 var app = express();
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
+
+// puerto sobre el que alojamos la API
 var port = process.env.PORT || 80;
 app.use(express.static(__dirname + '/public'));
 
-// model of my data
+// inicializamos las variables necesarias para JSON Web Tokens siguiendo el
+//    tutorial de 'Quick start' provisto por auth0.com
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: "https://adrianmorente.eu.auth0.com/.well-known/jwks.json"
+  }),
+  audience: 'dynasystem-users',
+  issuer: "https://adrianmorente.eu.auth0.com/",
+  algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
+
+// modelo de datos de los ejercicios guardados
 var exercise = require('./api/models/exercise');
 var exercises = new Array;
 
-// just a visit counter
-var visit_counter = 0;
-
-// index route
+// ruta índice por defecto
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
-  visit_counter += 1;
 });
 
-// status route, just trying that the service is running well
+// status route. Para testear el funcionamiento de la API y su estado 'running'
 app.get('/status', function(req, res){
   res.send('{ "status" : "OK" }');
-  visit_counter += 1;
 });
 
-// example of route
 app.get('/exercise', function(req, res){
-  visit_counter += 1;
+  // res.sendFile(__dirname + '/exercise.html');
   res.send("Welcome to Dynasystem - exercises! :D\n" +
-    "You\'re the number #" + visit_counter + " visitor!" +
     "Maybe you would like to add a exercise\' name to the URL...");
 });
 
-// list all exercises
 app.get('/exercises', function(req, res){
   res.send(exercises);
 });
 
-// delete an exercise
 app.delete('/exercises', function(req, res){
   exercises = [];
   res.sendStatus(200);
 });
 
-// example of route to exercise identified by name
 app.get('/exercise/:exerciseName', function(req, res){
   res.send("Welcome to Dynasystem - exercises! :D\n" +
-    "You're the number #" + visit_counter + " visitor!" +
     "\nAnd this is the exercise named: " + req.params.exerciseName);
 });
 
-// HTTP request to create an exercise
 app.put('/exercise/:type/:concentric_strength/:eccentric_strength/:initial_sense', function(req, res){
   var new_exercise = new exercise.Exercise(req.params.type,
     req.params.concentric_strength,
